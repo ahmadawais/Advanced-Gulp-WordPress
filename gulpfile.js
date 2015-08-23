139ec6f8ccb0f5a92b94ff13c412e7db68de500b
Ahmad Awais <mrahmadawais@gmail.com>
2015-08-23 23:47:27 +0500

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,33 +10,33 @@
 
 // Project configuration
 var project 		= 'neat', // Project name, used for build zip.
-	url 			= 'neat.dev', // Local Development URL for BrowserSync. Change as-needed.
-	bower 			= './assets/bower_components/'; // Not truly using this yet, more or less playing right now. TO-DO Place in Dev branch
-	build 			= './buildtheme/', // Files that you want to package into a zip go here
+	url 		= 'neat.dev', // Local Development URL for BrowserSync. Change as-needed.
+	bower 		= './assets/bower_components/'; // Not truly using this yet, more or less playing right now. TO-DO Place in Dev branch
+	build 		= './buildtheme/', // Files that you want to package into a zip go here
 	buildInclude 	= [
-						// include common file types
-						'**/*.php',
-						'**/*.html',
-						'**/*.css',
-						'**/*.js',
-						'**/*.svg',
-						'**/*.ttf',
-						'**/*.otf',
-						'**/*.eot',
-						'**/*.woff',
-						'**/*.woff2',
+				// include common file types
+				'**/*.php',
+				'**/*.html',
+				'**/*.css',
+				'**/*.js',
+				'**/*.svg',
+				'**/*.ttf',
+				'**/*.otf',
+				'**/*.eot',
+				'**/*.woff',
+				'**/*.woff2',
 
-						// include specific files and folders
-						'screenshot.png',
+				// include specific files and folders
+				'screenshot.png',
 
-						// exclude files and folders
-						'!node_modules/**/*',
-						'!assets/bower_components/**/*',
-						'!style.css.map',
-						'!assets/js/custom/*',
-						'!assets/css/patrials/*'
+				// exclude files and folders
+				'!node_modules/**/*',
+				'!assets/bower_components/**/*',
+				'!style.css.map',
+				'!assets/js/custom/*',
+				'!assets/css/patrials/*'
 
-					];
+			];
 
 // Load plugins
 	var gulp     = require('gulp'),
